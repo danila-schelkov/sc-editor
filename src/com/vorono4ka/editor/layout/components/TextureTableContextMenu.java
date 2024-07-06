8f02e04bd79738487caa9155eddc2e2295c2a4ac
Danila <crowo4ka@gmail.com>
2024-07-06 04:21:56 +0300

    refactor(textures): selecting row on right click if rows are not selected or others selected

--- src/com/vorono4ka/editor/layout/components/TextureTableContextMenu.java
+++ src/com/vorono4ka/editor/layout/components/TextureTableContextMenu.java
@@ -19,14 +19,17 @@ import java.nio.file.Path;
 
 public class TextureTableContextMenu extends ContextMenu {
     private final Table table;
+    private final JMenuItem exportButton;
 
     public TextureTableContextMenu(Table table) {
         super(table, null);
 
         this.table = table;
 
-        JMenuItem exportButton = this.add("Export", KeyEvent.VK_E);
+        exportButton = this.add("Export", KeyEvent.VK_E);
         exportButton.addActionListener(this::export);
+
+        this.popupMenu.addPopupMenuListener(new TablePopupMenuListener(this.popupMenu, table, this::onRowSelected));
     }
 
     private void export(ActionEvent actionEvent) {
@@ -61,5 +64,9 @@ public class TextureTableContextMenu extends ContextMenu {
                 }
             }
         });
+    }
+
+    private void onRowSelected(int rowIndex) {
+        exportButton.setEnabled(rowIndex != -1);
     }
 }
