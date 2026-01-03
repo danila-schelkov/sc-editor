db6649cadbcdc3c9b61cf160c4d7df32f084d5e1
kubune <klimaszewski2005@gmail.com>
2026-01-03 21:35:00 +0100

    Add copying child name in ChildrenTableContextMenu

--- src/main/java/dev/donutquine/editor/layout/contextmenus/ChildrenTableContextMenu.java
+++ src/main/java/dev/donutquine/editor/layout/contextmenus/ChildrenTableContextMenu.java
@@ -1,12 +1,14 @@
 package dev.donutquine.editor.layout.contextmenus;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.util.function.Function;
+
 import dev.donutquine.editor.Editor;
 import dev.donutquine.editor.layout.components.Table;
 import dev.donutquine.editor.layout.components.TablePopupMenuListener;
 import dev.donutquine.renderer.impl.swf.objects.DisplayObject;
 import dev.donutquine.renderer.impl.swf.objects.MovieClip;
-
-import java.util.function.Function;
 
 public class ChildrenTableContextMenu extends ContextMenu {
     private final Table table;
@@ -21,8 +23,35 @@ public class ChildrenTableContextMenu extends ContextMenu {
         this.add("Toggle visibility", event -> this.changeVisibility(child -> !child.isVisible()));
         this.add("Enable", event -> this.changeVisibility(child -> true));
         this.add("Disable", event -> this.changeVisibility(child -> false));
+        this.add("Copy child name", event -> this.copyChildName());
 
         this.popupMenu.addPopupMenuListener(new TablePopupMenuListener(this.popupMenu, table, rowIndex -> setMainComponentsEnabled(rowIndex != -1)));
+    }
+
+    private void copyChildName() {
+        int selectedRow = this.table.getSelectedRow();
+
+        String exportName = this.table.getValueAt(selectedRow, this.table.getColumnIndexByName("Name")).toString();
+        if (exportName != null) {
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+                new StringSelection(exportName),
+                null
+            );
+        }
+    }
+
+    // FEATURE IDEA: copy any cell value to clipboard
+    private void copyValueToClipboard() {
+        MovieClip movieClip = this.getMovieClip();
+        if (movieClip == null) return;
+
+        int selectedRow = this.table.getSelectedRow();
+        int selectedColumn = this.table.getSelectedColumn();
+        String cellName = this.table.getValueAt(selectedRow, selectedColumn).toString();
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+            new StringSelection(cellName),
+            null
+        );
     }
 
     public void changeVisibility(Function<DisplayObject, Boolean> visibilityFunction) {
