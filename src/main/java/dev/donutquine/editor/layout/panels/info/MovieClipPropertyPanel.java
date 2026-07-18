danila-schelkov/sc-editor

commit 9e0dcfb56ea827c3526b31480357d31283bf06f9

feat(movieclip): enable user to duplicate frames (#248)
* feat(movieclip): enable user to duplicate frames

Resolves #234

* feat(movieclip): enable user to duplicate frames using keyboard

src/main/java/dev/donutquine/editor/layout/panels/info/MovieClipPropertyPanel.java
@@ -2,8 +2,12 @@ package dev.donutquine.editor.layout.panels.info;
 
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.function.IntConsumer;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.DropMode;
@@ -12,7 +16,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import dev.donutquine.editor.layout.SupercellSWFLayoutController;
 import dev.donutquine.editor.layout.components.listeners.ChildrenListMouseListener;
 import dev.donutquine.editor.layout.components.listeners.FrameSelectionListener;
@@ -23,6 +30,7 @@ import dev.donutquine.editor.layout.components.tables.Table;
 import dev.donutquine.editor.layout.contextmenus.ChildrenTableContextMenu;
 import dev.donutquine.editor.layout.contextmenus.FrameElementTableContextMenu;
 import dev.donutquine.editor.layout.contextmenus.FrameTableContextMenu;
+import dev.donutquine.editor.layout.shortcut.KeyboardUtils;
 import dev.donutquine.editor.renderer.BlendMode;
 import dev.donutquine.renderer.impl.swf.objects.DisplayObject;
 import dev.donutquine.renderer.impl.swf.objects.MovieClip;
@@ -30,6 +38,9 @@ import dev.donutquine.swf.ScMatrixBank;
 import dev.donutquine.swf.movieclips.MovieClipFrame;
 
 public class MovieClipPropertyPanel extends JPanel {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieClipPropertyPanel.class);
+    private static final String DUPLICATE_FRAMES = "duplicateFrames";
+
     private final JTable timelineChildrenTable;
     private final JTable framesTable;
     private final JTable frameElementsTable;
@@ -62,11 +73,7 @@ public class MovieClipPropertyPanel extends JPanel {
             movieClip.forceSetFrame(index);
         });
 
-        Table framesTable = createFramesTable(framesTableModel);
-        framesTable.addSelectionListener(new FrameSelectionListener(framesTable, elementsCurrentFrameSetter));
-
-        new FrameTableContextMenu(framesTable, framesTableModel, swfLayoutController);
-        this.framesTable = framesTable;
+        this.framesTable = createFramesTable(framesTableModel, swfLayoutController, elementsCurrentFrameSetter);
 
         this.textInfoPanel = new JPanel();
 
@@ -94,9 +101,37 @@ public class MovieClipPropertyPanel extends JPanel {
         return table;
     }
 
-    private static Table createFramesTable(MovieClipFramesTableModel tableModel) {
+    private static Table createFramesTable(MovieClipFramesTableModel tableModel, SupercellSWFLayoutController swfLayoutController, IntConsumer elementsCurrentFrameSetter) {
         Table table = new Table(tableModel);
+
         table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+
+        AbstractAction duplicateAction = new AbstractAction("Duplicate") {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int firstIndex = table.getSelectedRow();
+                int elementCount = table.getSelectedRowCount();
+
+                try {
+                    tableModel.duplicate(firstIndex, elementCount);
+                } catch (IllegalArgumentException e) {
+                    LOGGER.warn(e.getLocalizedMessage());
+                }
+
+                // NOTE: Should we actually reset selection?
+                table.resetKeyboardActions();
+            }
+        };
+
+        table.getActionMap().put(DUPLICATE_FRAMES, duplicateAction);
+        KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_D, KeyboardUtils.ctrlButton());
+        duplicateAction.putValue(Action.ACCELERATOR_KEY, keyStroke);
+
+        table.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(keyStroke, DUPLICATE_FRAMES);
+
+        table.addSelectionListener(new FrameSelectionListener(table, elementsCurrentFrameSetter));
+
+        new FrameTableContextMenu(table, tableModel, swfLayoutController, duplicateAction);
 
         return table;
     }

src/main/java/dev/donutquine/editor/layout/components/tables/MovieClipFramesTableModel.java
@@ -1,9 +1,13 @@
 package dev.donutquine.editor.layout.components.tables;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.IntConsumer;
 import javax.swing.table.AbstractTableModel;
+import dev.donutquine.swf.Tag;
 import dev.donutquine.swf.movieclips.MovieClipFrame;
+import dev.donutquine.swf.movieclips.MovieClipFrameElement;
+import dev.donutquine.swf.movieclips.MovieClipFrame.Builder;
 
 public class MovieClipFramesTableModel extends AbstractTableModel {
     // private static final Logger LOGGER = LoggerFactory.getLogger(MovieClipFramesTableModel.class);
@@ -72,9 +76,46 @@ public class MovieClipFramesTableModel extends AbstractTableModel {
             throw new IllegalArgumentException("At least one frame must remain");
         }
 
+        int lastIndex = firstRow + rowCount;
         // TODO: make a command and add it to global UndoRedoManager
-        this.frames.subList(firstRow, firstRow + rowCount).clear();
-        this.fireTableRowsDeleted(firstRow, firstRow + rowCount);
+        this.frames.subList(firstRow, lastIndex).clear();
+        this.fireTableRowsDeleted(firstRow, lastIndex);
+
+        this.updateFrames();
+    }
+
+    public void duplicate(int firstRow, int rowCount) {
+        // TODO: make a command and add it to global UndoRedoManager
+        int lastIndex = firstRow + rowCount;
+
+        // NOTE: current strategy is to put all duplicated frames after selection (e.g. `|1 2 3| 4` -> `1 2 3 (1 2 3) 4`), 
+        //  but there is also an idea of putting duplicate frame after original frame (e.g. `|1 2 3| 4` -> `1 (1) 2 (2) 3 (3) 4`).
+        //
+        //  Also, we may allow multiple selections, then we can put all duplicates
+        //  - at the end                    (e.g. `|1 2 3| 4 5 |6 7| 8` -> `1 2 3 4 5 6 7 8 (1 2 3 6 7)`)
+        //  - after last selection          (e.g. `|1 2 3| 4 5 |6 7| 8` -> `1 2 3 4 5 6 7 (1 2 3 6 7) 8`) (bad),
+        //  - after theirs selection range  (e.g. `|1 2 3| 4 5 |6 7| 8` -> `1 2 3 (1 2 3) 4 5 6 7 (6 7) 8`),
+        //  - after their original frames   (e.g. `|1 2 3| 4 5 |6 7| 8` -> `1 (1) 2 (2) 3 (3) 4 5 6 (6) 7 (7) 8`)
+        List<MovieClipFrame> rangeToDuplicate = this.frames.subList(firstRow, lastIndex);
+
+        List<MovieClipFrame> duplicates = new ArrayList<>(rowCount);
+        for (MovieClipFrame frameToDuplicate : rangeToDuplicate) {
+            Builder builder = MovieClipFrame.builder();
+            builder.withLabel(frameToDuplicate.getLabel());
+            builder.setIncludeElements(frameToDuplicate.getTag() == Tag.MOVIE_CLIP_FRAME);
+
+            // TODO: optimize by adding setElements or/and addAllElements methods to builder. 
+            //  Frame elements are immutable so may be copied as references.
+            for (MovieClipFrameElement frameElement : frameToDuplicate.getElements()) {
+                builder.addElement(frameElement);
+            }
+
+            duplicates.add(builder.build());
+        }
+
+        this.frames.addAll(lastIndex, duplicates);
+
+        this.fireTableRowsInserted(lastIndex, lastIndex + rowCount);
 
         this.updateFrames();
     }

src/main/java/dev/donutquine/editor/layout/contextmenus/ContextMenu.java
@@ -35,6 +35,13 @@ public abstract class ContextMenu {
         return menu;
     }
 
+    public JMenuItem add(Action action) {
+        JMenuItem item = new JMenuItem(action);
+        mainComponents.add(item);
+        popupMenu.add(item);
+        return item;
+    }
+
     public JMenuItem add(String label) {
         JMenuItem item = new JMenuItem(label);
         mainComponents.add(item);

src/main/java/dev/donutquine/editor/layout/contextmenus/FrameTableContextMenu.java
@@ -1,6 +1,7 @@
 package dev.donutquine.editor.layout.contextmenus;
 
 import java.awt.event.ActionEvent;
+import javax.swing.Action;
 import javax.swing.JTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,7 @@ public class FrameTableContextMenu extends ContextMenu {
 	private final MovieClipFramesTableModel tableModel;
     private final SupercellSWFLayoutController swfLayoutController;
 
-    public FrameTableContextMenu(JTable table, MovieClipFramesTableModel tableModel, SupercellSWFLayoutController swfLayoutController) {
+    public FrameTableContextMenu(JTable table, MovieClipFramesTableModel tableModel, SupercellSWFLayoutController swfLayoutController, Action duplicateAction) {
         super(table, null);
 
         this.table = table;
@@ -26,6 +27,8 @@ public class FrameTableContextMenu extends ContextMenu {
 
         this.add("Goto and play", this::gotoAndPlay);
         this.add("Goto and stop", this::gotoAndStop);
+        this.addSeparator();
+        this.add(duplicateAction);
         this.addSeparator();
         this.add("Delete", this::delete);
 

commit 72bc260ea14b861ca904bc7160e07c96cecd0c1d

fix(movieclip): clear selection instead of resetting keyboard actions
Fixes bug introduced in #248

src/main/java/dev/donutquine/editor/layout/panels/info/MovieClipPropertyPanel.java
@@ -111,6 +111,7 @@ public class MovieClipPropertyPanel extends JPanel {
             public void actionPerformed(ActionEvent event) {
                 int firstIndex = table.getSelectedRow();
                 int elementCount = table.getSelectedRowCount();
+                if (elementCount < 1) return;
 
                 try {
                     tableModel.duplicate(firstIndex, elementCount);
@@ -119,7 +120,7 @@ public class MovieClipPropertyPanel extends JPanel {
                 }
 
                 // NOTE: Should we actually reset selection?
-                table.resetKeyboardActions();
+                table.clearSelection();
             }
         };
 