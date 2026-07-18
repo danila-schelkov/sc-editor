e454232891faa408b4900e2c06fc7934e8d22070
Danila Schelkov <54549682+danila-schelkov@users.noreply.github.com>
2026-07-18 15:29:39 +0300

    feat(movieclip): added keyboard shortcut for deleting frames (#250)

--- src/main/java/dev/donutquine/editor/layout/contextmenus/FrameTableContextMenu.java
+++ src/main/java/dev/donutquine/editor/layout/contextmenus/FrameTableContextMenu.java
@@ -3,26 +3,19 @@ package dev.donutquine.editor.layout.contextmenus;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
 import javax.swing.JTable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import dev.donutquine.editor.layout.SupercellSWFLayoutController;
 import dev.donutquine.editor.layout.components.tables.JTablePopupMenuListener;
-import dev.donutquine.editor.layout.components.tables.MovieClipFramesTableModel;
 import dev.donutquine.renderer.impl.swf.objects.DisplayObject;
 import dev.donutquine.renderer.impl.swf.objects.MovieClip;
 
 public class FrameTableContextMenu extends ContextMenu {
-    private final Logger LOGGER = LoggerFactory.getLogger(FrameTableContextMenu.class);
-
     private final JTable table;
-	private final MovieClipFramesTableModel tableModel;
     private final SupercellSWFLayoutController swfLayoutController;
 
-    public FrameTableContextMenu(JTable table, MovieClipFramesTableModel tableModel, SupercellSWFLayoutController swfLayoutController, Action duplicateAction) {
+    public FrameTableContextMenu(JTable table, SupercellSWFLayoutController swfLayoutController, Action duplicateAction, Action deleteAction) {
         super(table, null);
 
         this.table = table;
-        this.tableModel = tableModel;
         this.swfLayoutController = swfLayoutController;
 
         this.add("Goto and play", this::gotoAndPlay);
@@ -30,7 +23,7 @@ public class FrameTableContextMenu extends ContextMenu {
         this.addSeparator();
         this.add(duplicateAction);
         this.addSeparator();
-        this.add("Delete", this::delete);
+        this.add(deleteAction);
 
         this.popupMenu.addPopupMenuListener(new JTablePopupMenuListener(this.popupMenu, table, rowIndex -> setMainComponentsEnabled(rowIndex != -1)));
     }
@@ -59,17 +52,6 @@ public class FrameTableContextMenu extends ContextMenu {
 
         int frameIndex = this.table.getSelectedRow();
         movieClip.gotoAndStopFrameIndex(frameIndex);
-    }
-
-    private void delete(ActionEvent event) {
-        int firstIndex = this.table.getSelectedRow();
-        int elementCount = this.table.getSelectedRowCount();
-
-        try {
-            this.tableModel.delete(firstIndex, elementCount);
-        } catch (IllegalArgumentException e) {
-            LOGGER.warn(e.getLocalizedMessage());
-        }
     }
 
     private MovieClip getMovieClip() {
--- src/main/java/dev/donutquine/editor/layout/shortcut/KeyboardUtils.java
+++ src/main/java/dev/donutquine/editor/layout/shortcut/KeyboardUtils.java
@@ -5,6 +5,8 @@ import org.intellij.lang.annotations.MagicConstant;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
 
 public class KeyboardUtils {
     @MagicConstant(flagsFromClass = java.awt.event.InputEvent.class)
@@ -22,6 +24,14 @@ public class KeyboardUtils {
     @MagicConstant(flagsFromClass = java.awt.event.InputEvent.class)
     public static int ctrlButton() {
         return CTRL_BNT_KEY;
+    }
+
+    public static KeyStroke delete() {
+        if (SystemInfo.IS_MAC) {
+            return KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx());
+        } else {
+            return KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0);
+        }
     }
 
     private KeyboardUtils() {}
