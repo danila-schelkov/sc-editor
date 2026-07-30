4c280fca8fc9c92622102103f044c1ef5117ba5c
Danila Schelkov <me@donutquine.dev>
2026-07-31 02:50:27 +0300

    fix(ui): fix tab close icon colors on theme change

--- src/main/java/dev/donutquine/editor/gui/theme/ThemeManager.java
+++ src/main/java/dev/donutquine/editor/gui/theme/ThemeManager.java
@@ -1,12 +1,14 @@
 package dev.donutquine.editor.gui.theme;
 
+import javax.swing.UIManager;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.extras.FlatAnimatedLafChange;
+import com.formdev.flatlaf.icons.FlatTabbedPaneCloseIcon;
 
 public class ThemeManager {
-    // TODO: load default from settings, load default from system defaults
+    // TODO: load default from system defaults
     private static boolean isDark = false;
 
     public static void setTheme(boolean isDark) {
@@ -17,6 +19,12 @@ public class ThemeManager {
         } else {
             FlatLightLaf.setup();
         }
+
+        UIManager.put("TabbedPane.closeArc", 999);
+        UIManager.put("TabbedPane.closeCrossFilledSize", 5.5f);
+        // NOTE: must be here to actually update icon colors on each theme change
+        // NOTE: must be before updateUI to update icon in all existing tab components
+        UIManager.put("TabbedPane.closeIcon", new FlatTabbedPaneCloseIcon());
 
         FlatLaf.updateUI();
     }
